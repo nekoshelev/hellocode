04f6736890860221500ba6f14a892eaaa59aec06
«Nikolay <>
2022-06-28 22:21:59 +0600

    add Contition 005

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пльзователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же МАША!");
+}
+else
+{
+    Console.Write("Приве, ");
+    Console.WriteLine(username);
+}
