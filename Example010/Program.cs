81dd450d4d345bea5028333edc4a211ea15b042d
«Nikolay <>
2022-06-30 23:31:10 +0600

    Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 16, 23, 31, 34, 55, 69, 87, 38, 95 };
+
+int n = array.Length;
+int find = 87;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+
+}
